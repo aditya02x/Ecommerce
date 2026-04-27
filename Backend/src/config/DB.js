b93a565c448c67603dc482efb4a208b7fde2b3f7
Aditya Raj <adityaraj34@icloud.com>
2026-04-27 23:52:15 +0530

    dfddd

--- Backend/src/config/DB.js
+++ Backend/src/config/DB.js
@@ -5,7 +5,11 @@ dotenv.config()
 export  const connectDb = async ()=>{
    try {
     
-     const connect = await m
+     const connect = await mongoose.connect(process.env.MONGO_URL)
+    console.log("Mongo db has been connect sucessfyly")
+   } catch (error) {
+    console.log("Mongo db not connected")
+    
    }
 }
 

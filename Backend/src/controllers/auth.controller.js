c51c2d54098375ae56e3b6f2be68465282a54ea8
Aditya Raj <adityaraj34@icloud.com>
2026-04-30 23:39:51 +0530

    dagsds

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -63,6 +63,9 @@ export const loginUser = async (req, res) => {
             });
         }
 
+        // find user + include password
+        const user = await User.findOne({ email }).select("+password");
+
         if (!user) {
             return res.status(401).json({
                 message: "Invalid credentials"
